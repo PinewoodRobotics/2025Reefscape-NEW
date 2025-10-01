1c0ff1172ffec021faa8e5c503c084bef031482c
brigerodev <golden.notch.creeper@gmail.com>
2025-09-30 17:00:34 -0700

    update pos calib

--- src/main/java/frc/robot/constants/AlignmentConstants.java
+++ src/main/java/frc/robot/constants/AlignmentConstants.java
@@ -7,7 +7,7 @@ public class AlignmentConstants {
   public static class Coral {
     public static final Pose2d left = new Pose2d(0.6, -0.15, Rotation2d.fromDegrees(180)); // calculated left side of
                                                                                            // the coral
-    public static final Pose2d right = new Pose2d(0.6, 0.15, Rotation2d.fromDegrees(180)); // calculated right side of
+    public static final Pose2d right = new Pose2d(0.6, 0.17, Rotation2d.fromDegrees(180)); // calculated right side of
                                                                                            // the coral
 
     public static final Pose2d center = new Pose2d(0.7, 0, Rotation2d.fromDegrees(180)); // the center. The tag is being
