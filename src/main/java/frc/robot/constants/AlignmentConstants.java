dd6f84001f066e0be4d80ee226c796c3593d250a
brigerodev <golden.notch.creeper@gmail.com>
2025-10-06 15:35:33 -0700

    add deployment features

--- src/main/java/frc/robot/constants/AlignmentConstants.java
+++ src/main/java/frc/robot/constants/AlignmentConstants.java
@@ -5,9 +5,9 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public class AlignmentConstants {
   public static class Coral {
-    public static final Pose2d left = new Pose2d(0.6, -0.15, Rotation2d.fromDegrees(180)); // calculated left side of
+    public static final Pose2d left = new Pose2d(0.6, -0.16, Rotation2d.fromDegrees(180)); // calculated left side of
                                                                                            // the coral
-    public static final Pose2d right = new Pose2d(0.6, 0.17, Rotation2d.fromDegrees(180)); // calculated right side of
+    public static final Pose2d right = new Pose2d(0.6, 0.16, Rotation2d.fromDegrees(180)); // calculated right side of
                                                                                            // the coral
 
     public static final Pose2d center = new Pose2d(0.7, 0, Rotation2d.fromDegrees(180)); // the center. The tag is being
